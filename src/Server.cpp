49e0df6e43cec86850b1009ebe0fa4f1c5544dbb
Roman Shkurdyuk <romansh0106@gmail.com>
2025-12-07 19:15:17 +0300

    Implemented basic Server file creation and read logic

---
+++ src/Server.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "employee.h"
+
+int main() {
+    std::cout << "Server started." << std::endl;
+    std::cin.get(); // Чтобы консоль не закрылась сразу
+    return 0;
+}
